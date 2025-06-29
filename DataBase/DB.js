731bc17d1c83c1d1573c1a4c741012cf68635747
nishant23042002 <149838291+nishant23042002@users.noreply.github.com>
2025-06-29 20:54:53 +0530

    Simplifies DB connection string

--- DataBase/DB.js
+++ DataBase/DB.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
-import { DATABASE_NAME } from "../utils/constants.js";
 import dotenv from "dotenv"
 dotenv.config();
 
 export const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(`${process.env.MONGO_URI}/${DATABASE_NAME}`, {
+        const connection = await mongoose.connect(`${process.env.MONGO_URI}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
